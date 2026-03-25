e8a7b8c8a77c96673d20532390e6fb94e73256c6
Hamidd Hosseini <hamidd.hosseini@gmail.com>
2026-03-25 19:24:37 +0100

    refactor day 4

--- Day 4/first_part.py
+++ Day 4/first_part.py
@@ -1,46 +1,38 @@
+from typing import List
 
-file_name = 'input2.txt'
-with open(file_name) as f:
-    lines = f.read().splitlines()
+def load_grid_from_file(file_name: str) -> list[list[int]]:
+    with open(file_name) as f:
+        lines = f.read().splitlines()
+        grid = [[
+        1 if c == '@' else 0 for c in line
+    ] for line in lines]
 
-# TODO: Check grid
-grid = [[
-    1 if c == '@' else 0 for c in line
-] for line in lines]
+    return grid
 
-# for line in grid:
-#     print(line)
+def count_accessible_rolls(grid: List[List[int]]) -> int:
+    accessible_rolls = 0
+    for row in range(0, len(grid)):
+        for col in range(0, len(grid[row])):
+            if grid[row][col] == 1:
+                count = count_neigbours(grid, row, col) - 1  # Subtract 1 to exclude the cell itself
+                if count < 4:
+                    accessible_rolls += 1
+    return accessible_rolls
 
-result = [[0 for _ in line] for line in grid]
-unaccessible_rolls = 0
-accessible_rolls = 0
-for row in range(0, len(grid)):
-    for col in range(0, len(grid[row])):
-        if grid[row][col] == 1:
-            min_col = max(0, col - 1)
-            max_col = min(len(grid[row]) - 1, col + 1)
-            min_row = max(0, row - 1)
-            max_row = min(len(grid) - 1, row + 1)
-            count = 0
-            # print('-----------------------------')
-            # print(f'Checking cell ({row}, {col}) with neighbors from ({min_row}, {min_col}) to ({max_row}, {max_col})')
-            # for i in range(min_row, max_row + 1):
-            #     print(grid[i][min_col:max_col + 1])
-            for c in range(min_col, max_col + 1):
-                for r in range(min_row, max_row + 1):
-                    if grid[r][c] == 1:
-                        count += 1
-            # print(f'Cell ({row}, {col}) has {count} neighbors')
-            if count > 4:
-                # print(f'Found an unaccessable roll at ({row}, {col}) with {count} neighbors')
-                result[row][col] = 9
-                unaccessible_rolls += 1
-            else:
-                accessible_rolls += 1
-                result[row][col] = grid[row][col]
+def count_neigbours(grid: List[List[int]], row:int, col:int) -> int:
+    """
+    Counts the number of neighbours of a cell in the grid. Neighbours are defined as the 8 cells surrounding the cell, including diagonals.
+    """
+    min_col = max(0, col - 1)
+    max_col = min(len(grid[row]) - 1, col + 1)
+    min_row = max(0, row - 1)
+    max_row = min(len(grid) - 1, row + 1)
+    count = 0
+    for c in range(min_col, max_col + 1):
+        for r in range(min_row, max_row + 1):
+            if grid[r][c] == 1:
+                count += 1
+    return count
 
-print(f'Unaccessible rolls: {unaccessible_rolls}')
-print(f'Accessible rolls: {accessible_rolls}')
-
-# for line in result:
-#     print(line)
+grid = load_grid_from_file('input_final.txt')
+count_accessible_rolls(grid)
--- Day 4/second_part.py
+++ Day 4/second_part.py
@@ -1,13 +1,14 @@
 
-file_name = 'input2.txt'
-with open(file_name) as f:
-    lines = f.read().splitlines()
+from typing import List
 
-# TODO: Check grid
-grid = [[
-    1 if c == '@' else 0 for c in line
-] for line in lines]
+def load_grid_from_file(file_name: str) -> List[List[int]]:
+    with open(file_name) as f:
+        lines = f.read().splitlines()
+    grid = [[
+        1 if c == '@' else 0 for c in line
+    ] for line in lines]
 
+    return grid
 
 def count_and_remove_accessible_rolls(grid):
     accessible_rolls = 0
@@ -15,16 +16,8 @@ def count_and_remove_accessible_rolls(grid):
     for row in range(0, len(grid)):
         for col in range(0, len(grid[row])):
             if grid[row][col] == 1:
-                min_col = max(0, col - 1)
-                max_col = min(len(grid[row]) - 1, col + 1)
-                min_row = max(0, row - 1)
-                max_row = min(len(grid) - 1, row + 1)
-                count = 0
-                for c in range(min_col, max_col + 1):
-                    for r in range(min_row, max_row + 1):
-                        if grid[r][c] == 1:
-                            count += 1
-                if count <= 4:
+                count = count_neigbours(grid, row, col) - 1  # Remove the cell itself
+                if count < 4:
                     accessible_rolls += 1
                     grid_copy[row][col] = 9
 
@@ -36,7 +29,22 @@ def count_and_remove_accessible_rolls(grid):
             if grid_copy[row][col] == 9:
                 grid[row][col] = 0
 
-    # print(f'Accessible rolls found in this iteration: {accessible_rolls}')
     return accessible_rolls + count_and_remove_accessible_rolls(grid)
 
+def count_neigbours(grid: List[List[int]], row:int, col:int) -> int:
+    """
+    Counts the number of neighbours of a cell in the grid. Neighbours are defined as the 8 cells surrounding the cell, including diagonals.
+    """
+    min_col = max(0, col - 1)
+    max_col = min(len(grid[row]) - 1, col + 1)
+    min_row = max(0, row - 1)
+    max_row = min(len(grid) - 1, row + 1)
+    count = 0
+    for c in range(min_col, max_col + 1):
+        for r in range(min_row, max_row + 1):
+            if grid[r][c] == 1:
+                count += 1
+    return count
+
+grid = load_grid_from_file('input_final.txt')
 count_and_remove_accessible_rolls(grid)
